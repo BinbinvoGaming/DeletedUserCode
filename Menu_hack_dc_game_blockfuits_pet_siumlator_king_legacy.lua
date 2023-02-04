2616c1f25cbf7f3b7060d0ef60ee6436493f1ced
DeletedUserCodeblockfuits <123051097+DeletedUserCodeblockfuits@users.noreply.github.com>
2023-02-04 19:58:03 +0700

    Create Menu_hack_dc_game_blockfuits_pet_siumlator_king_legacy.lua

---
+++ Menu_hack_dc_game_blockfuits_pet_siumlator_king_legacy.lua
@@ -0,0 +1,28 @@
+--Blox Fruit
+
+repeat wait() until game:IsLoaded()
+
+local PlaceId = game.PlaceId
+
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+
+    
+loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/Blockfuits_hack_nhu_dac_cau.lua',true))()
+
+
+--King Legacy
+
+elseif PlaceId == 8304191830 then
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/King_Legacy_dac_cau.lua',true))()
+--Pet Siumlator X
+
+repeat wait() until game:IsLoaded()
+
+local PlaceId = game.PlaceId
+
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+
+    
+loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/PET_SIUMLATOR_DEP_TRAI_HON',true))()
+end
