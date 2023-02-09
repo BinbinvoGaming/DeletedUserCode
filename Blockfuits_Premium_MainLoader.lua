bb85f84b8139114e48b1931a7131891f7a2e530f
DeletedUserCodeblockfuits <123051097+DeletedUserCodeblockfuits@users.noreply.github.com>
2023-02-09 22:30:52 +0700

    Update and rename Blockfuits_Premium_MainLoader_Loading_Ui.lua to Blockfuits_Premium_MainLoader.lua

---
+++ Blockfuits_Premium_MainLoader.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet('https://pastebin.com/raw/eQw074ă))()loadstring(game:HttpGet('https://pastebin.com/raw/QDTV0mfk',true))()
--- Blockfuits_Premium_MainLoader_Loading_Ui.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet('https://pastebin.com/raw/eQw074aw',true))()loadstring(game:HttpGet('https://pastebin.com/raw/QDTV0mfk',true))()
