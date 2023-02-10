7a66a9ebeee4e22a19118e650a9e19cdcd86f852
DeletedUserCodeblockfuits <123051097+DeletedUserCodeblockfuits@users.noreply.github.com>
2023-02-10 20:06:05 +0700

    Create KL.lua

---
+++ KL.lua
@@ -0,0 +1,16 @@
+_, Protected_by_MoonSecV2, Discord = 'discord.gg/gQEH2uZxUk'_,
+
+
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+local Window = Library.CreateLib("Premium Binbinvoroblox11 Gui", "Ocean")
+local Tab = Window:NewTab("king legacy")
+local Section = Tab:NewSection("king legacy")
+Section:NewButton("Hoho Hub", "HoHo HUB", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
+end)
+Section:NewButton("One X Hub", "One X Hub", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xOne2/King-Legacy/main/README.md"))()
+end)
+Section:NewButton("MuKuro Hub ", "MuKuro Hub", function()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
+end)
