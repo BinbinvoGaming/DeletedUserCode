6f3f9b21a2547afdf8747fa77946b5d98f88b779
DeletedUserCodeblockfuits <123051097+DeletedUserCodeblockfuits@users.noreply.github.com>
2023-02-10 20:16:54 +0700

    Create MainPremiumLoader.lua

---
+++ MainPremiumLoader.lua
@@ -0,0 +1,9 @@
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/BF.lua',true))()
+elseif PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/KL.lua',true))()
+elseif PlaceId == 10321372166 or PlaceId == 6284583030 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/PSX.lua',true))()
+end
