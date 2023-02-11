7a2bb96977954f584e662f99894ce22464de7142
DeletedUserCodeblockfuits <123051097+DeletedUserCodeblockfuits@users.noreply.github.com>
2023-02-11 10:07:10 +0700

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,4 +1,4 @@
 (getgenv()).Key = "NON-VA-XANH-LAM-CON-ZAI-PREIUM"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/MainPremiumLoader.lua',true))()
-(getgenv()).Key = "PASTE KEY
+(getgenv()).Key = "PASTE KEY"
 game.Players.LocalPlayer:Kick("KEY DAU")
