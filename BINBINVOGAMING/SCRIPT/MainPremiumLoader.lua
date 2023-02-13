0cc0088d754a3c439914f8ba40ab1fa3867c10ed
DeletedUserCodeblockfuits <123051097+DeletedUserCodeblockfuits@users.noreply.github.com>
2023-02-13 19:07:06 +0700

    Update MainPremiumLoader.lua

--- BINBINVOGAMING/SCRIPT/MainPremiumLoader.lua
+++ BINBINVOGAMING/SCRIPT/MainPremiumLoader.lua
@@ -1,7 +1,7 @@
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/BF_Premium.lua',true))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/BINBINVOGAMING/SCRIPT/BF_Premium.lua',true))()
 elseif PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/KL.lua',true))()
 elseif PlaceId == 10321372166 or PlaceId == 6284583030 then
