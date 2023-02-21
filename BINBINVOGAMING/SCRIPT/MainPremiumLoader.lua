c51f662631eae3a5480d0aa7fd46823900efea02
BinbinvoGaming <123051097+DeletedUserCodeblockfuits@users.noreply.github.com>
2023-02-21 21:02:16 +0700

    Update MainPremiumLoader.lua

--- BINBINVOGAMING/SCRIPT/MainPremiumLoader.lua
+++ BINBINVOGAMING/SCRIPT/MainPremiumLoader.lua
@@ -5,18 +5,3 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
 elseif PlaceId == 10321372166 or PlaceId == 6284583030 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/DeletedUserCodeblockfuits/DeletedUserCode/main/PSX.lua',true))()
 end
-elseif PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
-    game.Players.LocalPlayer:Kick(" BinbinvoGaming HUB game not support, https://discord.gg/jUbSpzFcBY Binbinvogaming") end
-elseif PlaceId == 537413528 then
-    game.Players.LocalPlayer:Kick(" BinbinvoGaming HUB game not support, https://discord.gg/jUbSpzFcBY Binbinvogaming") end
-elseif PlaceId == 8396586868 or PlaceId == 9432106399 then
-    game.Players.LocalPlayer:Kick(" BinbinvoGaming HUB game not support, https://discord.gg/jUbSpzFcBY Binbinvogaming") end
-elseif PlaceId == 914010731 then
-    game.Players.LocalPlayer:Kick(" BinbinvoGaming HUB game not support, https://discord.gg/jUbSpzFcBY Binbinvogaming") end
-elseif PlaceId == 1730877806 or PlaceId == 3978370137 then
-    game.Players.LocalPlayer:Kick(" BinbinvoGaming HUB game not support, https://discord.gg/jUbSpzFcBY Binbinvogaming") end
-elseif PlaceId == 8304191830 or PlaceId == 8349889591 then
-    game.Players.LocalPlayer:Kick(" BinbinvoGaming HUB game not support, https://discord.gg/jUbSpzFcBY Binbinvogaming") end
-elseif PlaceId == 6918802270 then
-    game.Players.LocalPlayer:Kick(" BinbinvoGaming HUB game not support, https://discord.gg/jUbSpzFcBY Binbinvogaming") end
-end
