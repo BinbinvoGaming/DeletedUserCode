af6aa732eb418163c8c282994709a18ec4a5c55d
BinbinvoGaming <123051097+DeletedUserCodeblockfuits@users.noreply.github.com>
2023-02-24 09:24:48 +0700

    Create autoframbounty.lua

---
+++ Kaizenofficiall/BinbinvoGaming/AloneHub/autoframbounty.lua
@@ -0,0 +1,67 @@
+_G.Config = {
+    WebHook = "", -- Discord Webhook
+    Team = "Pirate", -- Pirate, Marine
+    Time = 120,
+    Invisible = false, -- Please use bundle: https://www.roblox.com/bundles/238/Man,
+    Melee = {
+        Use = true,
+        Z = {
+            Use = true,
+            Hold = 0.1
+        },
+        X = {
+            Use = true,
+            Hold = 0.1
+        },
+        C = {
+            Use = true,
+            Hold = 0.1
+        }
+    },
+    Sword = {
+        Use = true,
+        Z = {
+            Use = true,
+            Hold = 0.1
+        },
+        X = {
+            Use = true,
+            Hold = 0.1
+        }
+    },
+    Gun = {
+        Use = true,
+        Z = {
+            Use = true,
+            Hold = 0.1
+        },
+        X = {
+            Use = true,
+            Hold = 0.1
+        }
+    },
+    Fruit = {
+        Use = true,
+        Z = {
+            Use = true,
+            Hold = 0.1
+        },
+        X = {
+            Use = true,
+            Hold = 0.1
+        },
+        C = {
+            Use = true,
+            Hold = 0.1
+        },
+        V = {
+            Use = true,
+            Hold = 0.1
+        },
+        F = {
+            Use = false,
+            Hold = 0.1
+        }
+    }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AlongNgu/AloneHub/main/Bounty%20Beta.lua", true))()
