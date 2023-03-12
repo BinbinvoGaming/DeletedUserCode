e63a4317c8deefb9c344a4821febc1629075efed
BinbinvoGaming <123051097+BinbinvoGaming@users.noreply.github.com>
2023-03-12 10:39:37 +0700

    Create Menuchestfarm.lua

---
+++ BINBINVOGAMING/Menuchestfarm.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/chestfarmPremium.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/GiamLagmenu.lua'))()
