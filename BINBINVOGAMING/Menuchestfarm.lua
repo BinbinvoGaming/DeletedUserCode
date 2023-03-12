18b13425eb7d4b57ec1b322f1a129ed48353615d
BinbinvoGaming <123051097+BinbinvoGaming@users.noreply.github.com>
2023-03-12 10:51:24 +0700

    Update Menuchestfarm.lua

--- BINBINVOGAMING/Menuchestfarm.lua
+++ BINBINVOGAMING/Menuchestfarm.lua
@@ -1,2 +1,13 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/chestfarmPremium.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/GiamLagmenu.lua'))()
+game.StarterGui:SetCore("SendNotification", {
+    Title = "BinbinvoGaming HuB", 
+    Text = "chest farm them giam lag"
+})
+loadstring(game:HttpGet('https://pastebin.com/raw/UZZk7Nc8'))()
+
+wait(1.0)
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "BinbinvoGaming HuB", 
+    Text = "Thank you"
+})
+loadstring(game:HttpGet('https://pastebin.com/raw/YhxKYGE1'))()
