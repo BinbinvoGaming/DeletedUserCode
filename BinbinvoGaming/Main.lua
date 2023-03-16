7b71d9c17ad725a1350137664421d48a7e3f2f6a
BinbinvoGaming <123051097+BinbinvoGaming@users.noreply.github.com>
2023-03-16 13:25:47 +0700

    Update Main.lua

--- BinbinvoGaming/Main.lua
+++ BinbinvoGaming/Main.lua
@@ -1,5 +1,5 @@
 game:GetService("Lighting").ColorCorrection.Enabled = true
 game:GetService("Lighting").ColorCorrection.Brightness = -100
 wait(0.1)
-game.Players.LocalPlayer:Kick("Script Update")
+game.Players.LocalPlayer:Kick("Script BinbinvoGaming Update")
 setclipboard("https://discord.gg/bMad6XKd6d")
