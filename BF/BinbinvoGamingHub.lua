d566df87008cf0fdce8d907ddde75babc3a50065
BinbinvoGaming <123051097+BinbinvoGaming@users.noreply.github.com>
2023-03-21 16:21:55 +0700

    Update BinbinvoGamingHub.lua

--- BF/BinbinvoGamingHub.lua
+++ BF/BinbinvoGamingHub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/bloxfruit/04ce28e9722e9e299da6687f218e6d96c194c42d/BinbinvoGamingHubBF.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/bloxfruit/04ce28e9722e9e299da6687f218e6d96c194c42d/BinbinvoGamingHubBF.lua'))()loadstring(game:HttpGet('https://pastebin.com/raw/0KmBK9VX',true))()
