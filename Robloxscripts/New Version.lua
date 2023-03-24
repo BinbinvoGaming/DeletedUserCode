ca2cb790be8bd0708abfe5e0550b59cca066face
BinbinvoGaming <123051097+BinbinvoGaming@users.noreply.github.com>
2023-03-24 13:16:47 +0700

    Update New Version.lua

--- Robloxscripts/New Version.lua
+++ Robloxscripts/New Version.lua
@@ -44,6 +44,24 @@ Tab:AddButton({
   	end    
 })
 
+local Section = Tab:AddSection({
+	Name = "Auto Execute Script"
+})
+
+TabSettings:AddButton({
+	Name = "Main Script free",
+	Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/ScriptbyBinbinvogaming/Loader.lua',true))()
+  	end    
+})
+
+TabSettings:AddButton({
+	Name = "Main Auto farm",
+	Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/BinbinvoGamingCommmunty/main/Loader.lua',true))()
+  	end    
+})
+
 local CreditsTab = Window:MakeTab({
         Name = "Credits",
         Icon = "rbxassetid://4483345998",
@@ -51,5 +69,27 @@ local CreditsTab = Window:MakeTab({
 })
  
 CreditsTab:AddParagraph("Credits by Binbinvogaming","Script By BinbinvoGaming Free script https://scriptblox.com")
+
+local CreditsTab = Window:MakeTab({
+        Name = "Settings",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "Discord Server",
+	Callback = function()
+      		copy("https://discord.gg/srAJEg5Drq")
+              notice("Notification", "Copied!")
+  	end    
+})
+
+Tab:AddButton({
+	Name = "Youtube Channel",
+	Callback = function()
+      		copy("https://youtube.com/@binbinvogaming")
+              notice("Notification", "Copied!")
+  	end    
+})
 
 OrionLib:Init()
