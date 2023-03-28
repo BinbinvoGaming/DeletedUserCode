45b5a723d3a6e0f278d200ac21f0a8819e9e22e8
BinbinvoGaming <123051097+BinbinvoGaming@users.noreply.github.com>
2023-03-28 11:39:00 +0700

    Update Script.lua

--- BINBINVOGAMING/Script.lua
+++ BINBINVOGAMING/Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/MainPremiumBlox%20fruits%20petsimulator%20x.lua'))()
+loadstring(game:HttpGet('https://gitlab.com/DeletedUserCodeblockfuits/BinbinvoGaming/-/raw/main/BFLoader',true))()
