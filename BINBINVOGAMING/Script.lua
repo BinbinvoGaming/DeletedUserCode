eb80bd96dc88fcf5086e48a30661dcb572ae941d
BinbinvoGaming <123051097+BinbinvoGaming@users.noreply.github.com>
2023-03-29 13:50:16 +0700

    Update Script.lua

--- BINBINVOGAMING/Script.lua
+++ BINBINVOGAMING/Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/LoaderGame.lua))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/LoaderGame.lua'))()
