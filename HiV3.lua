563808dbe65029106d9f07291bf60a6e73d8e979
BinbinvoGaming <123051097+BinbinvoGaming@users.noreply.github.com>
2023-04-13 18:53:06 +0700

    Update HiV3.lua

--- HiV3.lua
+++ HiV3.lua
@@ -13,4 +13,4 @@ end
 
 warnmessage("BinbinvoGaming", "Script Made By BinbinvoGaming.", 10)
 task.wait(2.5)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/Bss/BinbinvoGaming.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/Hituibay'))()
